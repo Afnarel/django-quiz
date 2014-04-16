4bcee6147a7b1dc7562b0bfad4e21e096048d2cc
François Chapuis <afnarel@gmail.com>
2014-04-16 15:14:31 +0200

    Solving compatibility problem concerning from django.conf.urls.defaults

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls.defaults import urlpatterns, url
+except ImportError:
+    from django.conf.urls import urlpatterns, url
 
 urlpatterns = patterns('',
     # quiz base url    
