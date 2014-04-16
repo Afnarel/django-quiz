f5be6eb9235e03d1141c5195694a4582c01e5024
François Chapuis <afnarel@gmail.com>
2014-04-16 16:43:41 +0200

    admin.py is now conform to PEP8 according to flake8

--- quiz/admin.py
+++ quiz/admin.py
@@ -3,36 +3,36 @@ from django.contrib import admin
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from quiz.models import Quiz, Category, Progress, Question, Answer
 
+
 class QuestionInline(admin.TabularInline):
     model = Question.quiz.through
     filter_horizontal = ('content',)
 
 
 class AnswerInline(admin.TabularInline):
-    model = Answer 
+    model = Answer
 
 """
-below is from 
-http://stackoverflow.com/questions/11657682/django-admin-interface-using-horizontal-filter-with-inline-manytomany-field
+below is from
+http://stackoverflow.com/questions/11657682/django-admin-interface-using-horizontal-filter-with-inline-manytomany-field  # NOQA
 """
+
 
 class QuizAdminForm(forms.ModelForm):
     class Meta:
         model = Quiz
-    
+
     questions = forms.ModelMultipleChoiceField(
-                                               queryset=Question.objects.all(),
-                                               required=False,
-                                               widget=FilteredSelectMultiple(verbose_name=('Questions'),
-                                                                             is_stacked=False
-                                                                             )
-                                               )
-    
+        queryset=Question.objects.all(),
+        required=False,
+        widget=FilteredSelectMultiple(verbose_name=('Questions'),
+                                      is_stacked=False))
+
     def __init__(self, *args, **kwargs):
         super(QuizAdminForm, self).__init__(*args, **kwargs)
         if self.instance.pk:
             self.fields['questions'].initial = self.instance.question_set.all()
-    
+
     def save(self, commit=True):
         quiz = super(QuizAdminForm, self).save(commit=False)
         if commit:
@@ -42,36 +42,35 @@ class QuizAdminForm(forms.ModelForm):
             self.save_m2m()
         return quiz
 
+
 class QuizAdmin(admin.ModelAdmin):
     form = QuizAdminForm
-    
-    list_display = ('title', 'category', )
-    list_filter = ('category',)
-    search_fields = ('description', 'category', )
-    
 
-    
+    list_display = ('title', 'category',)
+    list_filter = ('category',)
+    search_fields = ('description', 'category',)
+
+
 class CategoryAdmin(admin.ModelAdmin):
-    search_fields = ('category', )
+    search_fields = ('category',)
+
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('content', 'category', )
+    list_display = ('content', 'category',)
     list_filter = ('category',)
-    fields = ('content', 'category', 'quiz', 'explanation' )
-    
-    search_fields = ('content', )
+    fields = ('content', 'category', 'quiz', 'explanation',)
+
+    search_fields = ('content',)
     filter_horizontal = ('quiz',)
 
-    
     inlines = [AnswerInline]
-    
-    
+
+
 class ProgressAdmin(admin.ModelAdmin):
     """
-    to do:
-            create a user section
+    TODO: create a user section
     """
-    search_fields = ('user', 'score', )
+    search_fields = ('user', 'score',)
 
 admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Category, CategoryAdmin)
