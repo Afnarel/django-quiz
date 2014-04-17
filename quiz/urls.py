6b30933f68b86768d5b7d210bae8f84b27506086
François Chapuis <afnarel@gmail.com>
2014-04-17 11:39:02 +0200

    urls.py is now PEP8 compliant

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,31 +3,29 @@ try:
 except ImportError:
     from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
-    # quiz base url    
-    url(r'^$', 'quiz.views.index'),   
+urlpatterns = patterns(
+    '',
+    # quiz base url
+    url(r'^$', 'quiz.views.index'),
 
-    # quiz category list    
-    url(r'^category/(?P<slug>[^\.]+)', 'quiz.views.view_category', name='view_quiz_category'),
-                       
-    #  progress 
+    # quiz category list
+    url(r'^category/(?P<slug>[^\.]+)', 'quiz.views.view_category',
+        name='view_quiz_category'),
+
+    #  progress
     url(r'^progress/$', 'quiz.views.progress'),
     url(r'^progress$', 'quiz.views.progress'),
 
-    
     #  passes variable 'quiz_name' to quiz_take view
     url(r'^(?P<quiz_name>[\w-]+)/$',
-        'quiz.views.quiz_take'),  #  quiz/
+        'quiz.views.quiz_take'),  # quiz/
 
     url(r'^(?P<quiz_name>[\w-]+)$',
-        'quiz.views.quiz_take'),  #  quiz
+        'quiz.views.quiz_take'),  # quiz
 
     url(r'^(?P<quiz_name>[\w-]+)/take/$',
-        'quiz.views.quiz_take'),  #  quiz/take/
+        'quiz.views.quiz_take'),  # quiz/take/
 
     url(r'^(?P<quiz_name>[\w-]+)take$',
-        'quiz.views.quiz_take')  #  quiz/take
-
-
-
+        'quiz.views.quiz_take')  # quiz/take
 )
